43e0e131b1506883575fbd8e90e26bd5e3fb9b3f
Gianmarco-Duilio <gianmarcoduilio@gmail.com>
2024-02-26 18:09:02 +0100

    commit 1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//CREAZIONE DI OGGETTI DI TIPO UTENTE E I SUOI ATTRIBUTI
+
 class Users {
   constructor(firstName, lastName, age, location) {
     this.name = firstName;
@@ -7,11 +9,15 @@ class Users {
   }
 }
 
+//OGGETTI
+
 const duilioGianmarco = new Users("Gianmarco", "Duilio", "27", "Anzio");
 console.log(duilioGianmarco);
 
 const marioRossi = new Users("Mario", "Rossi", "55", "Anzio");
 console.log(marioRossi);
+
+//CONFRONTO AGE
 
 function confrontoEta(utente1, utente2) {
   if (utente1.age > utente2.age) {
@@ -31,6 +37,8 @@ console.log(confrontoEta(duilioGianmarco, marioRossi));
 //   console.log("FORM INVIATO");
 // };
 
+//SECONDO ESERCIZIO
+
 class Pet {
   constructor(petName, ownerName, species, breed) {
     this.petName = petName;
@@ -39,15 +47,30 @@ class Pet {
     this.breed = breed;
   }
 }
+
+//ARRAY CONTENENTE I PADRONI E I LORO ANIMALI
+
 const myArray = [];
 
 document.getElementById("petForm").addEventListener("submit", function (event) {
+  //FA SI CHE NON SI REFRESHI LA PAGINA
+
   event.preventDefault();
+
   let petName = document.getElementById("petName").value;
   let ownerName = document.getElementById("ownerName").value;
   let species = document.getElementById("species").value;
   let breed = document.getElementById("breed").value;
   let pet = new Pet(petName, ownerName, species, breed);
   myArray.push(pet);
+  adPetList(pet);
   console.log(myArray);
 });
+
+function adPetList(pet) {
+  const listaAnimali = document.getElementById("listaAnimali");
+  const newLi = document.createElement("li");
+  newLi.innerText =
+    " Pet: " + pet.petName + " Nome: " + pet.ownerName + " Specie: " + pet.species + " Razza: " + pet.breed;
+  listaAnimali.appendChild(newLi);
+}
